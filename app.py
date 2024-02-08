197bcc6e14d3c2f91e885fa40bfc3923c90fb475
Tariq Bin Bashar <tariqbinbashar3203@gamil.com>
2024-02-08 12:35:50 -0600

    requirments added

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
+
 from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
